6e4dc783f6b52cfb59385049caad364fabeeacb6
benkirbycodes <benkirbycodes@gmail.com>
2020-02-07 13:01:57 -0700

    Auth Functions, SQL setup

--- db-setup.sql
+++ db-setup.sql
@@ -1,46 +1,46 @@
--- USE keepr13;
+USE keepr123;
 
--- CREATE TABLE vaults (
---     id int NOT NULL AUTO_INCREMENT,
---     name VARCHAR(255) NOT NULL,
---     description VARCHAR(255) NOT NULL,
---     userId VARCHAR(255),
---     INDEX userId (userId),  
---     PRIMARY KEY (id)
--- );
+CREATE TABLE vaults (
+    id int NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    description VARCHAR(255) NOT NULL,
+    userId VARCHAR(255),
+    INDEX userId (userId),  
+    PRIMARY KEY (id)
+);
 
--- CREATE TABLE keeps (
---     id int NOT NULL AUTO_INCREMENT,
---     name VARCHAR(255) NOT NULL,
---     description VARCHAR(255) NOT NULL,
---     userId VARCHAR(255),
---     img VARCHAR(255),
---     isPrivate TINYINT,
---     views INT DEFAULT 0,
---     shares INT DEFAULT 0,
---     keeps INT DEFAULT 0,
---     INDEX userId (userId),
---     PRIMARY KEY (id)
--- );
+CREATE TABLE keeps (
+    id int NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    description VARCHAR(255) NOT NULL,
+    userId VARCHAR(255),
+    img VARCHAR(255),
+    isPrivate TINYINT,
+    views INT DEFAULT 0,
+    shares INT DEFAULT 0,
+    keeps INT DEFAULT 0,
+    INDEX userId (userId),
+    PRIMARY KEY (id)
+);
 
--- CREATE TABLE vaultkeeps (
---     id int NOT NULL AUTO_INCREMENT,
---     vaultId int NOT NULL,
---     keepId int NOT NULL,
---     userId VARCHAR(255) NOT NULL,
+CREATE TABLE vaultkeeps (
+    id int NOT NULL AUTO_INCREMENT,
+    vaultId int NOT NULL,
+    keepId int NOT NULL,
+    userId VARCHAR(255) NOT NULL,
 
---     PRIMARY KEY (id),
---     INDEX (vaultId, keepId),
---     INDEX (userId),
+    PRIMARY KEY (id),
+    INDEX (vaultId, keepId),
+    INDEX (userId),
 
---     FOREIGN KEY (vaultId)
---         REFERENCES vaults(id)
---         ON DELETE CASCADE,
+    FOREIGN KEY (vaultId)
+        REFERENCES vaults(id)
+        ON DELETE CASCADE,
 
---     FOREIGN KEY (keepId)
---         REFERENCES keeps(id)
---         ON DELETE CASCADE
--- )
+    FOREIGN KEY (keepId)
+        REFERENCES keeps(id)
+        ON DELETE CASCADE
+);
 
 
 -- -- USE THIS LINE FOR GET KEEPS BY VAULTID
--- wwwsrc/src/authConfig.js
+++ wwwsrc/src/authConfig.js
@@ -1,3 +1,3 @@
-export const domain = "";
-export const audience = "";
-export const clientId = "";
+export const domain = "benkirbycodes.auth0.com";
+export const audience = "https://benkirbycodes-api.com";
+export const clientId = "elVnw8LFkDqF0ciWNFusNULTaKszAEwO";
